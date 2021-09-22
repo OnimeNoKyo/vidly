fa9db22456293da9f9e08a2782d19afd303e57c4
ianniau <aurelien.ianni@natixis.com>
2021-09-22 10:07:40 +0200

    feat: declare basepath as root application

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const root = express()
 const app = express()
 
 const hostname = process.env.HOST || '127.0.0.1'
 const port = process.env.PORT || 3000
+
+root.use('/vidly/v1', app)
 
 app.get('/', (req, res) => {
   const message = {
@@ -11,6 +14,14 @@ app.get('/', (req, res) => {
   return res.set('Content-Type', 'application/json').send(JSON.stringify(message))
 })
 
-app.listen(port, hostname, () => {
+app.route('/movieGenders')
+  .get((req, res, next) => {
+    //
+  })
+  .post((req, res, next) => {
+    //
+  })
+
+root.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`)
 })
