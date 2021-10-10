89454a0c8fcb82628e7c69b95557f669d59e5555
ianniau <aurelien.ianni@natixis.com>
2021-10-10 11:42:57 +0200

    fix: movieGender Repository Mock inherit from a Base repository
    allow "injection" test

--- src/repository/movieGenderRepository.js
+++ src/repository/movieGenderRepository.js
@@ -1,5 +1,7 @@
-class MovieGenderRepository {}
+function MovieGenderRepository () {}
 
 MovieGenderRepository.prototype.getAll = function () {}
+
+MovieGenderRepository.prototype.add = function () {}
 
 module.exports = MovieGenderRepository
