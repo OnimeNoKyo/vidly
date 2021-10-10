f0ec766daa0f70757fdbc44fef41b4bdb0dae42c
ianniau <aurelien.ianni@natixis.com>
2021-10-10 08:56:24 +0200

    feat: func to add a moviegender service

--- test/movieGenderServiceTest.js
+++ test/movieGenderServiceTest.js
@@ -8,4 +8,22 @@ describe('MovieGenderServiceTest', function () {
       assert.ok(movieGenders.length > 0)
     })
   })
+  describe('validate', () => {
+    it('should retrieve an error when movieGender doesnt have label attribute', async () => {
+      const { error } = movieGenderService.validate({ id: 1, name: 'test' })
+      assert.ok(error)
+    })
+    it('should retrieve no error when movieGender have label attribute', async () => {
+      const { error } = movieGenderService.validate({ id: 1, label: 'test' })
+      assert.ok(error === undefined)
+    })
+  })
+  describe('add', () => {
+    it('should add a new movieGender', async () => {
+      const initialLenght = movieGenderService.getAll().length
+      movieGenderService.add({ label: 'Cartoon' })
+      const newLenght = movieGenderService.getAll().length
+      assert.ok(newLenght > initialLenght)
+    })
+  })
 })
