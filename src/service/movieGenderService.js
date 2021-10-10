46b9e207d808620eeeacb5a267d4cf3bea4d56e1
ianniau <aurelien.ianni@natixis.com>
2021-10-10 08:56:24 +0200

    feat: func to add a moviegender service

--- src/service/movieGenderService.js
+++ src/service/movieGenderService.js
@@ -1,11 +1,28 @@
+const Joi = require('joi')
+
 const movieGenders = [
-  'Horror',
-  'Sci-Fi',
-  'Romantic'
+  { id: 1, label: 'Horror' },
+  { id: 2, label: 'Sci-Fi' },
+  { id: 3, label: 'Romantic' }
 ]
 
 function getAll () {
   return movieGenders
 }
 
-module.exports = { getAll }
+function add (newMovieGender) {
+  const lastAvailableId = movieGenders.flatMap((e) => {
+    return e.id
+  }).sort((a, b) => a - b)[movieGenders.length - 1]
+
+  movieGenders.push({ id: lastAvailableId + 1, label: newMovieGender.label })
+}
+
+function validate (movieGender) {
+  const schema = Joi.object({
+    label: Joi.string().min(3).required()
+  }).unknown(true)
+  return schema.validate(movieGender)
+}
+
+module.exports = { getAll, validate, add }
